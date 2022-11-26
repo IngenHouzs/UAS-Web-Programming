c909990e6022eaaff619ad96193085a755bf5740
Diangelion <bryanrichie92@gmail.com>
2022-11-26 10:56:33 +0700

    footer done?

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,3 +1,96 @@
-<div class="footer">
-    <h1>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam necessitatibus laudantium voluptates neque aspernatur possimus dolorem temporibus facere excepturi distinctio!</h1>
-</div>
+<footer>
+      <div class="footer">
+        <div class="container pt-3">
+          <div class="row">
+            <div class="col-xl-4 col-md-4 d-flex align-items-center justify-content-center">
+              <div class="row">
+                <div class="col d-flex align-items-center">
+                    <a href="/" class="mb-3 me-2 mb-md-0 text-decoration-none lh-1">
+                        <img
+                            src="images/LogoMedicWhite.png"
+                            alt="Tunas Mulia Montessari School"
+                        />
+                    </a>
+                    <span class="mb-3 mb-md-0">Tunas Mulia Montessari School</span>
+                </div>
+                <!-- <div class="col text-center">
+                  
+                  <p></p>
+                </div> -->
+              </div>
+            </div>
+            <div class="col-xl-5 col-md-5">
+              <div class="row">
+                <div class="col">
+                  <h5 id="footer-alamat">Alamat</h5>
+                </div>
+              </div>
+              <div class="row">
+                <div class="col">
+                  <p id="text-alamat">
+                    Gading Serpong, Jalan Boulevard Rusia Block CB 6 No. 57 Sektor 1 C, Pakulonan Bar., Kec. Klp. Dua, Kabupaten Tangerang, Banten 15810
+                  </p>
+                </div>
+              </div>
+            </div>
+            <div class="col-xl col-md">
+              <div class="row">
+                <div class="col">
+                  <h5 id="footer-kontak">Contact Us</h5>
+                </div>
+              </div>
+              <div class="row mb-2">
+                <div class="col d-flex align-items-center">
+                  @include('components.instagram')
+                  <a
+                    href="https://www.instagram.com/alivemedic.umn/"
+                    target="_blank"
+                    class="mx-2"
+                    >Instagram</a
+                  >
+                </div>
+              </div>
+              <div class="row mb-2">
+                <div class="col d-flex align-items-center">
+                  @include('components.line')
+                  <a
+                    href="https://line.me/R/ti/p/@470nogaq"
+                    target="_blank"
+                    class=" mx-2"
+                    >Line</a
+                  >
+                </div>
+              </div>
+              <div class="row mb-2">
+                <div class="col d-flex align-items-center">
+                  @include('components.email')
+                  <a
+                    href="https://mail.google.com/mail/?view=cm&fs=1&to=aliveumnmedic8.0@gmail.com"
+                    target="_blank"
+                    class=" mx-2"
+                  >
+                    Email</a
+                  >
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div class="copyright py-2">
+          <div class="container">
+            <div class="row">
+              <div class="col-md-12 text-center text-white">
+                <p>
+                  © 2022
+                  <a class="text-white" target="_blank" href="/">
+                    Tunas Mulia Montessari School</a
+                  >
+                  All Rights Reserved.
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </footer>
